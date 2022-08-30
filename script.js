82bf16e8b179f52c9194a28489fcda3ae08ffe27
Devos Julien <lhacktu@gmail.com>
2022-08-30 20:47:20 +0200

    Chef bug fix

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-let elem = document.getElementById('premium');
-let rect = elem.getBoundingClientRect();
-document.getElementById('chef').style.left = rect.left + 190 + "px";
+function chef() {
+    let elem = document.getElementById('premium');
+    let rect = elem.getBoundingClientRect();
+    document.getElementById('chef').style.left = rect.left + 190 + "px";
+}
+
+chef();
